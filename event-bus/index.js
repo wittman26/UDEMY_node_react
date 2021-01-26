bcef4d1887f6614e7a95bbd44c4f438084934588
Wittman Arley Gutierrez Torres <wa.gutierrez@soprasteria.com>
2021-01-26 12:57:07 +0100

    47. Missing Events Handling by Query Service Implementation. Add error handling in Event Bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,19 +5,37 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = []; // Stores all the events
+
 /* Basic Event Bus Implementation */
 app.post('/events', (req, res) => {
     const event = req.body;
 
+    events.push(event); // add the latest event
+
     // It is assumed that every call is succeeded. Missing error handling
     console.log('Event recieved: ', event);
-    axios.post('http://localhost:4000/events', event); //Posts
-    axios.post('http://localhost:4001/events', event); //Comments
-    axios.post('http://localhost:4002/events', event); //Query Call
-    axios.post('http://localhost:4003/events', event); //Moderation Call
+    
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    }); //Posts
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    }); //Comments
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    }); //Query Call
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message);
+    }); //Moderation Call
 
-    res.send({ status: 'OK'});
+    res.send({ status: 'OK' });
 })
+
+// Returns all the events in the time
+app.get('/events', (req, res) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
     console.log('listening in 4005')
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,48 +9,61 @@ app.use(cors());
 
 const posts = {};
 
+// Created to handle event separetly
+const handleEvent = (type, data) => {
+  // Conditions: PostCreated or CommentCreated
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
 // It gives back the posts object 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 // Request received from Event Bus
 app.post('/events', (req, res) => {
   console.log('SVC-QUERY: Event Recived: ', req.body.type);
-    const { type, data } = req.body;
+  const { type, data } = req.body;
 
-    // Conditions: PostCreated or CommentCreated
-    if (type === 'PostCreated') {
-      const { id, title } = data;
-  
-      posts[id] = { id, title, comments: [] };
-    }
-  
-    if (type === 'CommentCreated') {
-      const { id, content, postId, status } = data;
-  
-      const post = posts[postId];
-      post.comments.push({ id, content, status });
-    }
+  handleEvent(type, data);
 
-    if (type === 'CommentUpdated') {
-      const { id, content, postId, status } = data;
-  
-      const post = posts[postId];
-      const comment = post.comments.find(comment => {
-        return comment.id === id;
-      });
-  
-      comment.status = status;
-      comment.content = content;
-    }    
-  
-    console.log("Actual Posts State: " + JSON.stringify(posts));
-    console.log(posts);
-  
-    res.send({});
+  console.log("Actual Posts State: " + JSON.stringify(posts));
+  console.log(posts);
+
+  res.send({});
 });
 
-app.listen(4002, () => {
-    console.log('Listening on 4002');
-})
+app.listen(4002, async () => {
+  console.log('Listening on 4002');
+  // Returns all the events
+  const res = await axios.get('http://localhost:4005/events');
+
+  // Iterate the events
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+    handleEvent(event.type, event.data);
+  }
+});
