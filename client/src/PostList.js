6bbe87710301dcc1dc5795fe252e175791d2c9fd
Wittman Arley Gutierrez Torres <wa.gutierrez@soprasteria.com>
2021-01-29 13:06:54 +0100

    91. Update urls to make them uniques. Add client config file. Add path rules to ingress.

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,7 @@ export default () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4002/posts');
+        const res = await axios.get('http://posts.com/posts');
         console.log("Call to Posts(4000) removed!")
         setPosts(res.data);
     }
